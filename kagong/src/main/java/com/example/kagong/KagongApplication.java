5652c4f73a892a2a026576598469030e07e16a69
geoniklee <110885375+geoniklee@users.noreply.github.com>
2023-01-01 15:21:37 +0900

    123

---
+++ kagong/src/main/java/com/example/kagong/KagongApplication.java
@@ -0,0 +1,13 @@
+package com.example.kagong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KagongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KagongApplication.class, args);
+	}
+
+}
