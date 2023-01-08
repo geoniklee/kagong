74f421afbd97ed5246aeca11a33bd2c17d59fc06
Kyoungwoong <kyy980708@gmail.com>
2023-01-08 16:04:40 +0900

    영웅 수정

---
+++ kagong/src/test/java/com/example/kagong/repository/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.kagong.repository;
+
+import com.example.kagong.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    void 회원가입() {
+        Member member = new Member();
+        member.setName("memberA");
+        Long saveId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(saveId);
+
+        Assertions.assertThat(findMember.getMemberid()).isEqualTo(member.getMemberid());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void find() {
+    }
+}
